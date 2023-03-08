f32388225d67c81653af93fd623b4e53abb61da1
Amy Paschal <amypaschal@me.com>
2023-03-08 16:26:02 -0700

    initial cod for PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,14 +1,41 @@
-import csv
 import os
-import os.path
+import sys
+import pandas as pd
+import numpy as np
+import locale
+locale.setlocale(locale.LC_ALL, '')
 
+def generate_report(output_dev):
+    output_dev.write("Election Results\n------------------------- \n")
+    output_dev.write("Total Votes: " + str(votes_cast) + "\n")
+    output_dev.write("------------------------- \n")
+    for index, row in results_df.iterrows():
+        output_dev.write(index + ": " + "{:.3%}".format(row['Percentage Votes']) + " (" + str(int(row['Total Votes'])) + ")\n")
+    output_dev.write("------------------------- \n")
+    output_dev.write("Winner: " + winner + "\n")
+    output_dev.write("------------------------- \n")
+
+# get the data and load it into a data frame
 my_dir = os.getcwd()
 data_file = os.path.join(my_dir, "Resources/election_data.csv")
-print(data_file)
 
-with open(data_file, encoding='utf') as csvfile:
-   csvreader = csv.reader(csvfile, delimiter=',')
-   print(csvreader)
-    
-   for row in csvreader:
-       print(row)
+election_df = pd.read_csv(data_file, encoding="utf-8")
+
+
+# generate summary data
+votes_cast = len(election_df)
+results_df = pd.DataFrame()
+results_df['Total Votes'] = election_df['Candidate'].value_counts()
+results_df['Percentage Votes'] = results_df['Total Votes'] / votes_cast
+winner = results_df['Total Votes'].idxmax()
+
+
+
+# write report to the terminal
+generate_report(sys.stdout)
+
+# write report to an output file
+output_file = os.path.join(my_dir, "analysis/results.txt")
+with open(output_file, "w") as f:
+    generate_report(f)
+    f.close()
