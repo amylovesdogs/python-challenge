33fb0c1a6f698880e78033e59b12839489545668
Amy Paschal <amypaschal@me.com>
2023-03-13 09:16:47 -0600

    changed generate_report to take the summary dictionary as a parameter

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,13 +5,16 @@ import numpy as np
 import locale
 locale.setlocale(locale.LC_ALL, '')
 
-def generate_report(output_dev):
+
+# Generate a financial report to the output device given. It could be a file or the stdout (the terminal).
+
+def generate_report(output_dev, report_dict):
     output_dev.write("Financial Analysis\n------------------ \n")
-    output_dev.write("Total Months: " + str(summary["Total Months"]) + "\n")
-    output_dev.write("Total: " + locale.currency(summary["Total"], grouping=True) + "\n")
-    output_dev.write("Average Change: " + locale.currency(summary["Average Change"], grouping=True) + "\n")
-    output_dev.write("Greatest Increase in Profits: " + summary["Max Increase"][0] + " (" + locale.currency(summary["Max Increase"][1], grouping=True) + ")\n")
-    output_dev.write("Greatest Decrease in Profits: " + summary["Max Decrease"][0] + " (" + locale.currency(summary["Max Decrease"][1], grouping=True) + ")\n")
+    output_dev.write("Total Months: " + str(report_dict["Total Months"]) + "\n")
+    output_dev.write("Total: " + locale.currency(report_dict["Total"], grouping=True) + "\n")
+    output_dev.write("Average Change: " + locale.currency(report_dict["Average Change"], grouping=True) + "\n")
+    output_dev.write("Greatest Increase in Profits: " + report_dict["Max Increase"][0] + " (" + locale.currency(report_dict["Max Increase"][1], grouping=True) + ")\n")
+    output_dev.write("Greatest Decrease in Profits: " + report_dict["Max Decrease"][0] + " (" + locale.currency(report_dict["Max Decrease"][1], grouping=True) + ")\n")
 
 # get the data and load it into a data frame
 my_dir = os.getcwd()
@@ -25,6 +28,7 @@ bank_df = pd.read_csv(data_file, encoding="utf-8")
 delta = np.array([float("nan")])
 delta_max = 0
 delta_min = 0
+#loop through the records. Skip the first row of data since there is no "previous" month to calculate from
 for i in range(1,len(bank_df)):
     delta = np.append(delta, (bank_df["Profit/Losses"][i] - bank_df["Profit/Losses"][i-1]))
     if delta[i] > delta_max:
@@ -33,6 +37,7 @@ for i in range(1,len(bank_df)):
     if delta[i] < delta_min:
         delta_min = delta[i]
         month_min = bank_df["Date"][i]
+# add the change array as a column to the dataframe
 bank_df["Monthly Change"] = delta.tolist()
 
 
@@ -47,10 +52,10 @@ summary["Max Decrease"] = [month_min, delta_min]
 
 
 # write report to the terminal
-generate_report(sys.stdout)
+generate_report(sys.stdout,summary)
 
 # write report to an output file
 output_file = os.path.join(my_dir, "analysis/summary.txt")
 with open(output_file, "w") as f:
-    generate_report(f)
+    generate_report(f,summary)
     f.close()
